1204c631e2b56511c2e96f2c739420899bde4f5f
R0g3r-SH <rogelio84@hotmail.com>
2024-02-16 15:21:10 -0600

    yol

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-require('dotenv').config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import axios from 'axios';
+import { OpenAI } from 'openai';
+
+const client = new OpenAI({apiKey:'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'});
+import dotenv from 'dotenv';
+//import { json } from 'body-parser';
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -46,7 +53,7 @@ app.post('/login', async (req, res) => {
     bcrypt.compare(password, user.password, (err, result) => {
         if (err || !result) return res.status(401).json({ message: 'Invalid username or password' });
 
-        res.json({ user: " user logged in successfully" });
+        res.json({ user: user._id });
     });
 });
 
@@ -84,16 +91,19 @@ app.post('/register', async (req, res) => {
 // add sell arrticles to the database
 
 app.post('/sell', async (req, res) => {
-    const { article, price, quantity, user_that_sell_id } = req.body;   
+    const { article, price, stock, user_that_sell_id , img_url , descripcion } = req.body;
     try {
         // Create a new delivery
         const newSell = new Sell({
             article,
             price,
-            quantity,
-            user_that_sell_id
+            stock,
+            user_that_sell_id,
+            active : true,
+            img_url,
+            descripcion,
+            ubicacion
         });
-
         // Save the delivery to the database
         await newSell.save();
 
@@ -102,12 +112,15 @@ app.post('/sell', async (req, res) => {
         console.error('Error adding sell:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
+    
 });
 
 
 // add buy arrticles to the database
 app.post('/buy', async (req, res) => {
+
     const { article_id, quantity, user_that_bought_id } = req.body;
+
     try {
         // Create a new buy
         const newBuy = new Buy({
@@ -115,7 +128,6 @@ app.post('/buy', async (req, res) => {
             quantity,
             user_that_bought_id
         });
-
         // Find the corresponding sell entry
         const sell = await Sell.findOne({ article_id });
 
@@ -139,10 +151,12 @@ app.post('/buy', async (req, res) => {
         await newBuy.save();
 
         res.status(201).json({ message: 'Buy added successfully' });
+
     } catch (error) {
         console.error('Error adding buy:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
+
 });
 
 // Get all sell entries
@@ -159,7 +173,9 @@ app.get('/sell', async (req, res) => {
 
 // Get all buy entries by user id
 app.get('/buy/:user_id', async (req, res) => {
+
     const { user_id } = req.params;
+
     try {
         const buys = await Buy.find({ user_that_bought_id: user_id });
         res.json(buys);
@@ -167,6 +183,148 @@ app.get('/buy/:user_id', async (req, res) => {
         console.error('Error getting buys:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
+    
+});
+
+
+async function createRequest() {
+
+    console.log('Fetching data...');
+    const qs = new URLSearchParams();
+    qs.append('companyName', 'value');
+
+    const url = 'https://api.careeronestop.org/v1/jobsearch/pyHnNawZ1iCib4A/developer/10001/5/0/0/0/10/0';
+
+    try {
+
+        const response = await axios.get(url, {
+            params: qs,
+            headers: {
+                'Authorization': 'Bearer cCq+9/BdLlJyDvISrGhp0y8uUb8RVqZEu8dO1iHaVpLgcMy9KvckliL430njmjHAMGa1jSxNiYmiJUF+wT0VPQ==',
+                'Content-Type': 'application/json',
+            }
+        });
+
+        if (response.status === 200) {
+            const result = response.data;
+            console.log(result);
+        } else {
+            console.error('Failed to fetch:', response.statusText);
+        }
+
+    } catch (error) {
+        console.error('Error during fetch:', error.message);
+    }
+
+
+}
+
+
+
+app.post('/generate-ai-resume', async (req, res) => {
+
+    console.log(req.body)
+
+    const prompt = `based on this profile information ${req.body} create resume for the same person dont aleterate the user date improved whit the spected fields of the schema be creative in the about section and job description`
+
+    const schema = {
+        "type": "object",
+        "properties": {
+            'name': {
+                "type": "string"
+            },
+            "email": {
+                "type": "string"
+            },
+            "profession": {
+                "type": "string"
+            },
+            "positions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "name_of_the_position": {
+                            "type": "string"
+                        },
+                        "company": {
+                            "type": "string"
+                        },
+                        "start_date": {
+                            "type": "string"
+                        },
+                        "end_date": {
+                            "type": "string"
+                        },
+                        "description": {
+                            "type": "string"
+                        }
+                    },
+
+                    "required": ["name_of_the_position", "company", "start_date", "end_date", "description"]
+                }
+            },
+            "education": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "degree": {
+                            "type": "string"
+                        },
+                        "institution": {
+                            "type": "string"
+                        },
+                        "start_date": {
+                            "type": "string"
+                        },
+                        "end_date": {
+                            "type": "string"
+                        }
+                    },
+                    "required": ["degree", "institution", "start_date", "end_date"]
+                }
+            },
+            "about_me": {
+                "type": "string"
+            },
+            "skills": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            },
+
+        },
+        "required": ["slides"]
+    }
+
+    client.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", "content": "You are a helpful assistant  expert in cv and resume" },
+            { role: "user", content: prompt }],
+        functions: [{ name: "set_recipe", parameters: schema }],
+        function_call: { name: "set_recipe" }
+
+    }).then((completion) => {
+        // Note the updated location for the response
+        const response = completion.choices[0].message.function_call.arguments
+
+       const responseParsed = JSON.parse(response)
+       
+       responseParsed.name = req.body.name
+       responseParsed.email = req.body.email
+       responseParsed.education = req.body.school
+       responseParsed.positions = req.body.positions
+        
+
+
+        res.send(responseParsed )
+    })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
 
