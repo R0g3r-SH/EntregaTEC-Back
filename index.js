e8c4f3c3467328e663ff43849da1b16ef2f738de
R0g3r-SH <rogelio84@hotmail.com>
2024-03-14 16:44:36 -0600

    sells

--- index.js
+++ index.js
@@ -160,19 +160,24 @@ app.put('/sell_status/:id', async (req, res) => {
     //change the  fiield active value 
     const { id } = req.params;
     const { active } = req.body;
+
     try {
         const sell = await Sell.findById(id);
+
         if (!sell) {
             return res.status(404).json({ message: 'Sell not found' });
         }
+
         sell.active = active;
         await sell.save();
+
         res.json({ message: 'Sell updated successfully' });
     } catch (error) {
         console.error('Error updating sell:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
 
 // add buy arrticles to the database
 app.post('/buy', async (req, res) => {
@@ -216,6 +221,8 @@ app.post('/buy', async (req, res) => {
     }
 
 });
+
+
 //edit a sell entry
 app.put('/sell/:id', async (req, res) => {
     const { id } = req.params;
@@ -247,6 +254,11 @@ app.get('/sell', async (req, res) => {
     try {
         const sells = await Sell.find();
         res.json(sells);
+
+        ///return only the sells whit active status 
+        const sells = await Sell.find({active:true});
+        res.json(sells);
+
     } catch (error) {
         console.error('Error getting sells:', error);
         res.status(500).json({ message: 'Internal server error' });
@@ -269,9 +281,12 @@ app.get('/buy/:user_id', async (req, res) => {
     
 });
 
+
 //delete a sell entry
 app.delete('/sell/:id', async (req, res) => {
+    
     const { id } = req.params;
+
     try {
         const sell = await Sell.findById(id);
         if (!sell) {
@@ -283,152 +298,8 @@ app.delete('/sell/:id', async (req, res) => {
         console.error('Error deleting sell:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
+
 });
-
-
-
-async function createRequest() {
-
-    console.log('Fetching data...');
-    const qs = new URLSearchParams();
-    qs.append('companyName', 'value');
-
-    const url = 'https://api.careeronestop.org/v1/jobsearch/pyHnNawZ1iCib4A/developer/10001/5/0/0/0/10/0';
-
-    try {
-
-        const response = await axios.get(url, {
-            params: qs,
-            headers: {
-                'Authorization': 'Bearer cCq+9/BdLlJyDvISrGhp0y8uUb8RVqZEu8dO1iHaVpLgcMy9KvckliL430njmjHAMGa1jSxNiYmiJUF+wT0VPQ==',
-                'Content-Type': 'application/json',
-            }
-        });
-
-        if (response.status === 200) {
-            const result = response.data;
-            console.log(result);
-        } else {
-            console.error('Failed to fetch:', response.statusText);
-        }
-
-    } catch (error) {
-        console.error('Error during fetch:', error.message);
-    }
-
-
-}
-
-
-
-app.post('/generate-ai-resume', async (req, res) => {
-
-    console.log(req.body)
-
-    const prompt = `based on this profile information ${req.body} create resume for the same person dont aleterate the user date improved whit the spected fields of the schema be creative in the about section and job description`
-
-    const schema = {
-        "type": "object",
-        "properties": {
-            'name': {
-                "type": "string"
-            },
-            "email": {
-                "type": "string"
-            },
-            "profession": {
-                "type": "string"
-            },
-            "positions": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "name_of_the_position": {
-                            "type": "string"
-                        },
-                        "company": {
-                            "type": "string"
-                        },
-                        "start_date": {
-                            "type": "string"
-                        },
-                        "end_date": {
-                            "type": "string"
-                        },
-                        "description": {
-                            "type": "string"
-                        }
-                    },
-
-                    "required": ["name_of_the_position", "company", "start_date", "end_date", "description"]
-                }
-            },
-            "education": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "degree": {
-                            "type": "string"
-                        },
-                        "institution": {
-                            "type": "string"
-                        },
-                        "start_date": {
-                            "type": "string"
-                        },
-                        "end_date": {
-                            "type": "string"
-                        }
-                    },
-                    "required": ["degree", "institution", "start_date", "end_date"]
-                }
-            },
-            "about_me": {
-                "type": "string"
-            },
-            "skills": {
-                "type": "array",
-                "items": {
-                    "type": "string"
-                }
-            },
-
-        },
-        "required": ["slides"]
-    }
-
-    client.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "system", "content": "You are a helpful assistant  expert in cv and resume" },
-            { role: "user", content: prompt }],
-        functions: [{ name: "set_recipe", parameters: schema }],
-        function_call: { name: "set_recipe" }
-
-    }).then((completion) => {
-        // Note the updated location for the response
-        const response = completion.choices[0].message.function_call.arguments
-
-       const responseParsed = JSON.parse(response)
-       
-       responseParsed.name = req.body.name
-       responseParsed.email = req.body.email
-       responseParsed.education = req.body.school
-       responseParsed.positions = req.body.positions
-        
-
-
-        res.send(responseParsed )
-    })
-        .catch((error) => {
-            console.log(error);
-        });
-});
-
-
-
 
 
 
